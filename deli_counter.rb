7f46f66ae460623012e7ccede77040966c188f20
Jess Rezac <jess.rezac@gmail.com>
2019-05-02 23:26:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -55,14 +55,14 @@ binding.pry
 
 
 # katz_deli = []
-# 
+#
 # def line(katz_deli)
 #   ## Build the line method that shows everyone their current place in the line.
 #   # If there is nobody in line, it should say "The line is currently empty.".
-# 
+#
 #   if katz_deli.size > 0
 #     line_order = "The line is currently:"
-# 
+#
 #     katz_deli.each_with_index do |person, place_in_line|
 #       place_in_line += 1
 #       line_order << " #{place_in_line}. #{person}"
@@ -72,7 +72,7 @@ binding.pry
 #     puts "The line is currently empty."
 #   end
 # end
-# 
+#
 # def take_a_number(katz_deli, new_person)
 #   ## Build a method that a new customer will use when entering the deli.
 #   # The take_a_number method should accept two arguments,
@@ -84,15 +84,15 @@ binding.pry
 #   place_in_line = katz_deli.index(new_person)
 #   place_in_line += 1
 #   puts "Welcome, #{new_person}. You are number #{place_in_line} in line."
-# 
+#
 # end
-# 
+#
 # def now_serving(katz_deli)
 #   ## Build the now_serving method which should call out (i.e. puts)
 #   # the next person in line and then remove them from the front.
 #   # If there is nobody in line, it should call out (puts)
 #   # that "There is nobody waiting to be served!".
-# 
+#
 #   if katz_deli.size > 0
 #     person_being_served = katz_deli.first
 #     puts "Currently serving #{person_being_served}."
@@ -102,7 +102,3 @@ binding.pry
 #     puts "There is nobody waiting to be served!"
 #   end
 # end
-
-
-
-
